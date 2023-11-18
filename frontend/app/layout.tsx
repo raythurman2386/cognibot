1716c5b8c7d6664844f64139e9b7415acccd4aa0
Ray Thurman <raymondthurman5@gmail.com>
2023-11-17 18:18:19 -0600

    font, smooth scroll, header placed in layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import Link from "next/link";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={GeistSans.className}>
         <nav className="bg-gray-800 p-4">
           <div className="container mx-auto flex justify-between items-center">
             <Link href="/" className="text-2xl font-bold">
@@ -33,6 +31,19 @@ export default function RootLayout({
             </div>
           </div>
         </nav>
+        <header className="text-center py-16">
+          <h1 className="text-4xl font-bold">Welcome to CogniBot</h1>
+          <p className="mt-4 text-lg">
+            Your friendly Discord bot powered by OpenAI's API for text and image
+            generation.
+          </p>
+          <Link
+            href="/#subscription"
+            className="mt-8 inline-block bg-blue-500 text-white px-6 py-3 rounded-full text-lg font-semibold hover:bg-blue-600"
+          >
+            Subscribe to Updates
+          </Link>
+        </header>
         {children}
         <footer className="bg-gray-800 text-gray-400 py-4">
           <div className="container mx-auto text-center">
--- frontend/app/terms/page.tsx
+++ frontend/app/terms/page.tsx
@@ -3,25 +3,40 @@ import Image from "next/image";
 export default function Terms() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <section id="terms" className="container mx-auto my-8">
+      <section id="terms" className="container mx-auto my-8">
         <h1 className="text-3xl font-bold mb-4">CogniBot - Terms of Service</h1>
 
-        <p className="mb-4">By using CogniBot, you agree to comply with and be bound by the following terms and conditions:</p>
+        <p className="mb-4">
+          By using CogniBot, you agree to comply with and be bound by the
+          following terms and conditions:
+        </p>
 
         <h2 className="text-xl font-semibold mb-2">1. Use of the Service</h2>
-        <p className="mb-4">You must use CogniBot in compliance with all applicable laws and regulations.</p>
+        <p className="mb-4">
+          You must use CogniBot in compliance with all applicable laws and
+          regulations.
+        </p>
 
         <h2 className="text-xl font-semibold mb-2">2. Data Privacy</h2>
-        <p className="mb-4">We respect your privacy. Our Privacy Policy outlines how we collect, use, and protect your data.</p>
+        <p className="mb-4">
+          We respect your privacy. Our Privacy Policy outlines how we collect,
+          use, and protect your data.
+        </p>
 
         <h2 className="text-xl font-semibold mb-2">3. Prohibited Activities</h2>
-        <p className="mb-4">You are prohibited from engaging in any activities that may harm the service or other users.</p>
+        <p className="mb-4">
+          You are prohibited from engaging in any activities that may harm the
+          service or other users.
+        </p>
 
         <h2 className="text-xl font-semibold mb-2">4. Changes to Terms</h2>
-        <p className="mb-4">We reserve the right to modify these terms at any time. You are responsible for reviewing updates.</p>
+        <p className="mb-4">
+          We reserve the right to modify these terms at any time. You are
+          responsible for reviewing updates.
+        </p>
 
         <p className="mt-8">Last updated: [Date]</p>
-    </section>
+      </section>
     </main>
   );
 }
