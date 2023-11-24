fd9459b55fd9e67842bc7d9d539a4c03e6b45e33
raythurman2386 <raymondthurman5@gmail.com>
2023-11-23 20:06:29 -0600

    refactored anthropic into it's own utils

--- utils/openai.py
+++ utils/openai.py
@@ -2,7 +2,6 @@ import os
 
 from dotenv import load_dotenv
 from db.database import add_message, get_chat_log
-from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
 from openai import OpenAI
 
 load_dotenv()
@@ -12,17 +11,9 @@ env_vars = {
     "image_model": os.environ.get("IMAGE_MODEL") or "dall-e-3",
     "image_size": os.environ.get("IMAGE_SIZE") or "1024x1024",
     "image_quality": os.environ.get("IMAGE_QUALITY") or "standard",
-    "claude_model": os.environ.get("CLAUDE_MODEL") or "claude-2",
-    "anthropic_key": os.environ.get("ANTHROPIC_API_KEY"),
 }
 
 client = OpenAI()
-
-
-anthropic = Anthropic(
-    # defaults to os.environ.get("ANTHROPIC_API_KEY")
-    api_key=env_vars["anthropic_key"],
-)
 
 
 class CustomError(Exception):
@@ -68,34 +59,6 @@ def ask_gpt(question):
             model=env_vars["gpt_model"], messages=chat_log, temperature=0.1
         )
         answer = response.choices[0].message.content
-
-        # Insert the bot's response into the database
-        add_message("assistant", answer)
-
-        return answer
-    except Exception as e:
-        return handle_error(e)
-
-
-def ask_claude(question):
-    try:
-        if len(question) == 0:
-            raise CustomError("Please provide a question for Claude!")
-
-        # Insert the user's message into the database
-        add_message("user", question)
-
-        # Retrieve the chat log from the database
-        chat_log = get_chat_log()
-
-        response = anthropic.completions.create(
-            model=env_vars["claude_model"],
-            max_tokens_to_sample=300,
-            temperature=0.1,
-            # messages=chat_log,
-            prompt=f"You are Claude, a helpful pair programming bot who provides useful suggestions and explanations to programmers.{HUMAN_PROMPT}{question}{AI_PROMPT}",
-        )
-        answer = response.completion
 
         # Insert the bot's response into the database
         add_message("assistant", answer)
