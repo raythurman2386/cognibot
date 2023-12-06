1a2670f7a5760f664cbfe2e76d932d28cb3228a7
raythurman2386 <raymondthurman5@gmail.com>
2023-12-06 09:33:52 -0600

    formatted files

--- utils/openai.py
+++ utils/openai.py
@@ -63,7 +63,7 @@ def ask_gpt(question):
     except Exception as e:
         app_logger.error("GPT generation encountered an error: {e}")
         return handle_error(e)
-    
+
 
 def ask_vision():
     pass
