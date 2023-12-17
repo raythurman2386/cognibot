944ed78d041444e75359829442da587e09d446a9
raythurman2386 <raymondthurman5@gmail.com>
2023-12-16 20:17:41 -0600

    led cahnges

--- led_script.py
+++ led_script.py
@@ -1,12 +1,18 @@
 import RPi.GPIO as GPIO
 import subprocess
+import time
+
+GPIO.setmode(GPIO.BCM)
 
 led_pin = 18
 GPIO.setup(led_pin, GPIO.OUT) 
-
 cognibot = subprocess.check_output("systemctl is-active cognibot", shell=True)
 
 if cognibot == "active":
    GPIO.output(led_pin, GPIO.HIGH) # Turn on 
 else:
    GPIO.output(led_pin, GPIO.LOW) # Turn off
+
+time.sleep(5)
+
+
