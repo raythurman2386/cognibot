43043ec6d2e8639c61036479d88ed90588ab50f6
raythurman2386 <raymondthurman5@gmail.com>
2024-01-13 18:13:46 -0600

    updated onjoin message to users

--- cogs/greetings.py
+++ cogs/greetings.py
@@ -19,11 +19,22 @@ class Greetings(commands.Cog):
     @discord.user_command()
     async def greet(self, ctx, member: discord.Member):
         await ctx.defer(ephemeral=True)
-        await ctx.followup.send(f"{ctx.author.mention} says hello to {member.mention}!")
+        await member.send(f"{ctx.author.mention} says hello to {member.mention}!")
+        await ctx.followup.send(f"PM Sent to {member}")
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        await member.send("Welcome to the server!")
+        await member.send(
+            f"""Welcome to the server, {member.mention}! 👋
+
+We're so glad you decided to join us. Please take a moment to review the #welcome-and-rules channel to get started.
+
+If you have any questions, feel free to ask one of our moderators.
+
+We hope you enjoy your time here! Don't hesitate to introduce yourself in #introductions once you're settled in.
+
+- The Ravenwood Team"""
+        )
 
 
 def setup(bot):  # this is called by Pycord to setup the cog
