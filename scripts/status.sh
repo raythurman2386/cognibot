a84c9be844cb8e667c98966133687fd0b63c42e2
raythurman <raymondthurman5@gmail.com>
2024-08-28 22:27:18 -0500

    🐛 Status doesn't need to state it's not in the current directory

--- scripts/status.sh
+++ scripts/status.sh
@@ -4,7 +4,6 @@ EXPECTED_DIR="cognibot"
 CURRENT_DIR=${PWD##*/}
 
 if [ "$CURRENT_DIR" != "$EXPECTED_DIR" ]; then
-    echo "Not in the $EXPECTED_DIR directory. Changing directory..."
     cd ./cognibot
 fi
 
